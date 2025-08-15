3d34abc72deff7fa7e579d92cbc3bd9ab3d25963
Luiz Poderoso <luizpoderoso72@outlook.com>
2025-08-15 20:58:10 -0300

    feat: shadcn and pages structure

---
+++ src/app/profile/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Página Dashboard</h1>
+      <p>Essá é a página de Dashboard</p>
+    </div>
+  );
+}
---
+++ src/app/profile/import/page.tsx
@@ -0,0 +1,8 @@
+export function Import() {
+  return (
+    <div>
+      <h1>Página de importação</h1>
+      <p>Importação de dados</p>
+    </div>
+  );
+}
