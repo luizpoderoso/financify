30b3aa890abaa0f9783e40f8de448821afcc0bcd
Luiz Poderoso <luizpoderoso72@outlook.com>
2025-08-16 08:05:07 -0300

    feat: scaffold login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,66 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Página de login</h1>
-      <p>Aqui ficará a página de login</p>
+    <div className="flex justify-center py-20">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your username below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="your_username"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  {/*<a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>*/}
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="**********"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Sign Up
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
