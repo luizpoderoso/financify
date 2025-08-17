6d82142b5e81326b4698d06c959a8e34e13c4c7c
Luiz Poderoso <luizpoderoso72@outlook.com>
2025-08-17 13:26:11 -0300

    fix: fix layout children type

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,7 +1,11 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export default async function ProfileLayout({ children }) {
+export default async function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const session = await auth();
 
   if (!session?.user?.id) {
